a0a5dd76688d3f49d1086718b327048b92886bfe
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-11-20 02:16:18 +0300

    Work started on a web server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,17 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        Frontend frontend = new Frontend();
+
+        Server server = new Server(8080);
+        ServletContextHandler ctx = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(ctx);
+        ctx.addServlet(new ServletHolder(frontend), "/authform");
+
+        server.start();
+        server.join();
+    }
 }
