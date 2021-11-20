9ba599092f7f98fe225a4a4b5a46ebed7c953ecb
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-11-21 01:31:29 +0300

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,34 @@
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestsServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Frontend frontend = new Frontend();
+
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
 
         Server server = new Server(8080);
-        ServletContextHandler ctx = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        server.setHandler(ctx);
-        ctx.addServlet(new ServletHolder(frontend), "/authform");
+        server.setHandler(context);
 
         server.start();
+        System.out.println("Server started");
         server.join();
     }
 }
+
+
+//    public static void main(String[] args) throws Exception {
+//        Frontend frontend = new Frontend();
+//
+//        Server server = new Server(8080);
+//        ServletContextHandler ctx = new ServletContextHandler(ServletContextHandler.SESSIONS);
+//        server.setHandler(ctx);
+//        ctx.addServlet(new ServletHolder(frontend), "/authform");
+//
+//        server.start();
+//        server.join();
+//    }
